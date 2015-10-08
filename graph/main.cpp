3af75343eac5cd8922828d07f990f15cd7fed789
mhty <olivergrane@gmail.com>
2015-10-08 16:31:53 +0300

    Initial Commit

---
+++ graph/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graph
+//
+//  Created by Dmitry Mikhaylovich on 08.10.15.
+//  Copyright © 2015 Dmitry Mikhaylovich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
